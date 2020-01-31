137799055411392d5f1c46bb8d650d52d8cf258e
Rafael Escobar <rjescobar83@gmail.com>
2020-01-30 22:12:03 -0600

    Separate user setings in config.json file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,9 @@ package com.company;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.net.URL;
@@ -15,20 +17,39 @@ import java.util.Scanner;
 public class Main {
 
     private static String endPoint = "https://www.googleapis.com/customsearch/v1?";
-    private static String apiKey = "AIzaSyDkBAolh2nEbGAxAcifKETCVH7VJ1R4-WI";
-    private static String cx = "010552605714490616389:7iliw8fr8vz";
 
     public static void main(String[] args) throws Exception {
+
+        String apiKey = null, cx = null;
+
+        // Leer archivo de configuracion.
+        try {
+            JsonObject json = (JsonObject) JsonParser.parseReader(new FileReader("config.json"));
+            apiKey = json.get("key").getAsString();
+            cx = json.get("cx").getAsString();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return;
+        }
+
+        if ((apiKey == null) || (cx == null)) {
+            throw new IllegalArgumentException("El archivo de configuracion no contiene valores validos.");
+        }
+
+
         // Ingresar objetivo.
         Scanner in = new Scanner(System.in);
         String target = in.nextLine();
         String targetFormatted = target.replaceAll(" ", "+");
 
-        targetFormatted =  "site:linkedin.com" + "+" + targetFormatted;
+        targetFormatted = "site:linkedin.com" + "+" + targetFormatted;
 
         // Creamos la url de ejemplo.
         String queryString = endPoint + "key=" + apiKey +
                 "&cx=" + cx + "&q=" + targetFormatted + "&num=10&start=1";
+
+        System.out.println(queryString);
 
         // Generamos el objeto de la conexion.
         URL url = new URL(queryString);
@@ -47,9 +68,9 @@ public class Main {
         PrintWriter printWriter = new PrintWriter(fileWriter);
 
         // Escribimos en el archivo TXT
-        for(Resultado resultado : resultados) {
+        for (Resultado resultado : resultados) {
             System.out.println(resultado);
-            printWriter.println(String.format("%s, %s", resultado.title,  resultado.link));
+            printWriter.println(String.format("\"%s\", \"%s\"", resultado.title, resultado.link));
         }
         // Cerrar el buffer y escribir.
         printWriter.close();
